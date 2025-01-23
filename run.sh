3c1dd39c280435c34ca8c177c775eda0c1213861
MingLib <xiaoming.dlut.edu@gmail.com>
2025-01-23 16:32:57 +0800

    update

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ attack_model_name='mnasnet0_5'
 
 echo "attack on ${attack_model_name} ensemble_size=20 image_num=1000"
 
-CUDA_VISIBLE_DEVICES=0 python run_attack.py --n_wb 20 --victim $attack_model_name --loss_name cw
+CUDA_VISIBLE_DEVICES=0 python qt-attack.py --n_wb 20 --victim $attack_model_name --loss_name cw
 
 echo "attack on ${attack_model_name} ensemble_size=20 image_num=1000"
